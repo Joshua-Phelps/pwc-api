38cc49c515f72e669cefe4d2c84c6119536233d1
jugtuttle <jugtuttle@gmail.com>
2020-07-15 15:35:23 -0700

    added gallery contorller actions & private params

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,2 +1,31 @@
 class GalleriesController < ApplicationController
+  def index
+    @galleries = Gallery.all
+    render :json => @galleries
+  end
+
+  def show
+    @gallery = Gallery.find(gallery_params[:id])
+    render :json => @gallery
+  end 
+
+  def create
+    @gallery.new(gallery_params)
+    if @gallery.save
+      render :json => @gallery
+    else 
+      render :json => {status: 422, error: 'Unable to create gallery'}
+    end 
+  end
+
+  def show
+    gallery = Gallery.find(gallery_params[:id])
+    render :json => {gallery: gallery, current_paintings: gallery.current_paintings }
+  end
+
+  private
+
+  def gallery_params
+    params.require(:gallery).permit(:id, :address, :name, :max_paintings)
+  end 
 end
