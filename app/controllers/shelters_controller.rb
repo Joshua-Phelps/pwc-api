b726c7acb7d28a1cf528cede01ef4489affea70f
jugtuttle <jugtuttle@gmail.com>
2020-07-15 15:40:19 -0700

    private params and controller actions for shelters

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,2 +1,21 @@
 class SheltersController < ApplicationController
+  def index
+    @shelters = Shelter.all
+    render :json => @shelters
+  end
+
+  def show
+    @shelter = Shelter.find(params[:id])
+    if @shelter
+      render :json => @shelter
+    else 
+      render json => {status: 404, message: 'Shelter not found!'}
+    end 
+  end
+
+  private
+
+  def shelter_params
+    params.require(:shelter).permit(:id, :external_id, :name)
+  end 
 end
