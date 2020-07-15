ed1181898d76dde87e23d4b202af4597ba9091ad
jugtuttle <jugtuttle@gmail.com>
2020-07-15 15:38:57 -0700

    added paint_loc & painting controller actions & private parms

--- app/controllers/paint_locations_controller.rb
+++ app/controllers/paint_locations_controller.rb
@@ -1,2 +1,19 @@
 class PaintLocationsController < ApplicationController
+
+  def index
+    @paint_locs = PaintLocation.all
+    render :json => @paint_locs
+  end
+
+  def show 
+    @paint_loc = PaintLocation.find(params[:id])
+    render :json => @paint_loc, serializer: CompletePaintLocationSerializer
+  end 
+
+  private 
+
+  def paint_locations_params
+    params.require(:paint_location).permit(:id, :name, :address)
+  end 
+
 end
--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,2 +1,26 @@
 class PaintingsController < ApplicationController
+  
+  def create
+    @painting.new(painting_params)
+    if @painting.save
+      render :json => @painting
+    else 
+      render :json => {status: 422, error: 'Unable to create painting!'}
+    end 
+  end
+
+  def show
+    @painting = Painting.find(painting_params[:id])
+    if @painting
+      render :json => @painting
+    else 
+      render :json => {status: 422, error: 'Unable to find painting!'}
+    end 
+  end
+
+  private
+
+  def painting_params
+    params.require(:painting).permit(:id, :animal_id, :paint_location_id, :painting_url, :status, :painter)
+  end 
 end
